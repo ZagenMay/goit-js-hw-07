25a0ef8b793b94c9f37a4cb1d037aa227cea3b1e
ZagenMay <zagenhaften@gmail.com>
2021-11-01 05:14:43 +0200

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryBox = document.querySelector(".gallery");
+galleryBox.addEventListener("click", onImageOpen);
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+galleryBox.innerHTML = markup;
+
+const instance = basicLightbox.create(`<img src="" />`, {
+  onShow: () => {
+    window.addEventListener("keydown", onImageClose);
+  },
+  onClose: () => {
+    window.removeEventListener("keydown", onImageClose);
+  },
+});
+
+function onImageOpen(event) {
+  event.preventDefault();
+  instance.element().querySelector("img").src = event.target.dataset.source;
+  instance.show();
+}
+
+function onImageClose({ key }) {
+  if (key === "Escape") {
+    instance.close();
+    return;
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryBox = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>
+`
+  )
+  .join("");
+galleryBox.innerHTML = markup;
+
+const lightBox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
